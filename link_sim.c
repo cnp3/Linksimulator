cnp3/Linksimulator

commit d35d44122396f8db3f95e3696af6b552c965a103

Update link_sim.c

link_sim.c
@@ -464,7 +464,7 @@ int main(int argc, char **argv)
 		switch (opt) {
 #define _READINT_CAP(x, y, lim) case x: y = atoi(optarg) % lim; break;
 #define _READINT(x, y) _READINT_CAP(x, y, INT_MAX)
-			_READINT_CAP('p', port, SHRT_MAX)
+			_READINT_CAP('p', port, (1<<16) - 1)
 			_READINT_CAP('P', forward_port, SHRT_MAX)
 			_READINT('d', delay)
 			_READINT('j', jitter)

commit 9e5c249afb86e94254ce3516adab1c2df6a93c63

link_sim: fix broken port parsing Commits be0eae2 and d35d441 broke the port parsing. It removes one unity from the given one in arg.

link_sim.c
@@ -464,8 +464,8 @@ int main(int argc, char **argv)
 		switch (opt) {
 #define _READINT_CAP(x, y, lim) case x: y = atoi(optarg) % lim; break;
 #define _READINT(x, y) _READINT_CAP(x, y, INT_MAX)
-			_READINT_CAP('p', port, (1 << 16) - 1)
-			_READINT_CAP('P', forward_port, (1 << 16) - 1)
+			_READINT_CAP('p', port, (1 << 16))
+			_READINT_CAP('P', forward_port, (1 << 16))
 			_READINT('d', delay)
 			_READINT('j', jitter)
 			_READINT_CAP('e', err_rate, 101)